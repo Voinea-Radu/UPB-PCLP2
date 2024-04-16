c4305ba8b0601d101f891f3e56dbc0ca4a730a63
Voinea Radu <contact@voinearadu.com>
2024-04-16 19:35:04 +0300

    Task 1

--- src/task-1/check_permission.asm
+++ src/task-1/check_permission.asm
@@ -5,26 +5,44 @@ extern ant_permissions
 extern printf
 global check_permission
 
+section .data
+    ant_index dd 0
+    requested_permissions dd 0
+
 section .text
 
 check_permission:
-    ;; DO NOT MODIFY
     push    ebp
     mov     ebp, esp
     pusha
 
     mov     eax, [ebp + 8]  ; id and permission
     mov     ebx, [ebp + 12] ; address to return the result
-    ;; DO NOT MODIFY
-   
-    ;; Your code starts here
 
-    ;; Your code ends here
-    
-    ;; DO NOT MODIFY
+    ;; Get the ant id
+    mov ecx, eax
+    and ecx, 0xFF000000
+    shr ecx, 24
+    mov [ant_index], ecx
 
+    ;; Get the requested permissions
+    mov edx, eax
+    and edx, 0x00FFFFFF
+    mov [requested_permissions], edx
+
+    ;; Get the ant permissions
+    mov eax, [ant_permissions + ecx * 4]
+
+    mov DWORD[ebx], 1 ; assume the ant has the requested permissions
+
+    ;; Check if the ant has the requested permissions
+    and eax, [requested_permissions]
+    cmp eax, [requested_permissions]
+    jz end
+
+    mov DWORD[ebx], 0 ; the ant does not have the requested permissions
+
+end:
     popa
     leave
     ret
-    
-    ;; DO NOT MODIFY
--- src/task-2/subtask1.asm
+++ src/task-2/subtask1.asm
@@ -2,25 +2,37 @@
 
 ; declare your structs here
 
+section .data
+    loop_index_1 dw 0
+    loop_index_2 dw 0
+    requests dw 0
+    requests_length dw 0
+
 section .text
     global sort_requests
     extern printf
 
 sort_requests:
-    ;; DO NOT MODIFY
     enter 0,0
     pusha
 
     mov ebx, [ebp + 8]      ; requests
     mov ecx, [ebp + 12]     ; length
-    ;; DO NOT MODIFY
 
-    ;; Your code starts here
+    mov [requests], ebx
+    mov [requests_length], ecx
 
-    ;; Your code ends here
+    mov [loop_index_1], ebx
+    mov [loop_index_2], ebx
 
-    ;; DO NOT MODIFY
+    loop_1:
+        loop_2:
+            mov eax, loop_index_2
+            mov ebx, [requests + eax] ; admin
+            ;mov ecx, [requests + eax + 1] ; priority
+            PRINTF32 `Is admin? %d\n\x0`, ebx
+            ;PRINTF32 `Priority: %d\n\x0`, ecx
+
     popa
     leave
     ret
-    ;; DO NOT MODIFY
