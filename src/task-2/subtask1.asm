8c43d5b537d9362110d480383945be4d7d6d8309
Voinea Radu <contact@voinearadu.com>
2024-04-17 04:39:17 +0300

    Task 2 / Sub-Task 1 (10/20)

--- src/task-2/subtask1.asm
+++ src/task-2/subtask1.asm
@@ -26,6 +26,7 @@ section .data
 section .text
     global sort_requests
     extern printf
+    extern debug_print
 
 sort_requests:
     enter 0,0
@@ -43,7 +44,9 @@ sort_requests:
 
     mov DWORD [loop_index_1], 0
     loop_1:
-        mov DWORD [loop_index_2], 0
+        mov eax, [loop_index_1]
+        inc eax
+        mov DWORD [loop_index_2], eax
         loop_2:
             ;; Read the request with index loop_index_1
             mov ebx, [loop_index_1]
@@ -105,47 +108,94 @@ sort_requests:
 
             ;; DEBUG ;; TODO Delete
 
-            PRINTF32 `[BEFORE 1] Is admin? %hhd\n\x0`, [admin_1]
-            PRINTF32 `[BEFORE 1] Priority: %hhu\n\x0`, [priority_1]
-            PRINTF32 `[BEFORE 1] Passkey: %hu\n\x0`, [passkey_1]
-            PRINTF32 `[BEFORE 1] Usename: %s\n\x0`, username_1
-            PRINTF32 `\nVS\n\n\x0`
+            ; PRINTF32 `Before\n\x0`
+            ; PRINTF32 `Is admin? %hhd\n\x0`, [admin_1]
+            ; PRINTF32 `Priority: %hhu\n\x0`, [priority_1]
+            ; PRINTF32 `Passkey: %hu\n\x0`, [passkey_1]
+            ; PRINTF32 `Usename: %s\n\x0`, username_1
+            ; PRINTF32 `\nVS\n\n\x0`
 
-            PRINTF32 `[BEFORE 2] Is admin? %hhd\n\x0`, [admin_2]
-            PRINTF32 `[BEFORE 2] Priority: %hhu\n\x0`, [priority_2]
-            PRINTF32 `[BEFORE 2] Passkey: %hu\n\x0`, [passkey_2]
-            PRINTF32 `[BEFORE 2] Usename: %s\n\x0`, username_2
+            ; PRINTF32 `Is admin? %hhd\n\x0`, [admin_2]
+            ; PRINTF32 `Priority: %hhu\n\x0`, [priority_2]
+            ; PRINTF32 `Passkey: %hu\n\x0`, [passkey_2]
+            ; PRINTF32 `Usename: %s\n\x0`, username_2
 
-            PRINTF32 `\n\n\n\n\x0`
+            ; PRINTF32 `\n\n\n\n\x0`
 
             ;; Compare the requests' admin status
+            ; PRINTF32 `Comapring {%d} with \x0`, [loop_index_1]
+            ; PRINTF32 `{%d}\n\x0`, [loop_index_2]
+
             mov al, BYTE [admin_1]
             cmp al, BYTE [admin_2]
 
-            jl admin_1_lower
-            jg admin_1_greater
+            jb admin_1_lower
+            je admin_1_equal
+            ja admin_1_greater
+
             jmp compare_end
 
-            admin_1_lower:
-                PRINTF32 `SWAP FOR ADMIN STATUS\n\n\n\x0`
+            admin_1_lower: ; admin1 < admin2
+                ; PRINTF32 `admin_1 < admin_2. Swapping...\n\x0`
                 jmp swap_requests
-            admin_1_greater:
+            admin_1_equal: ; admin1 == admin2
+                ; PRINTF32 `admin_1 == admin_2. Correct order. Checking for priority\n\x0`
                 ;; Compare the requests' priorities
                 mov al, BYTE [priority_1]
                 cmp al, BYTE [priority_2]
 
-                jl priority_1_lower
-                jg priority_1_greater
+                jb priority_1_lower
+                je priority_1_equal
+                ja priority_1_greater
                 jmp compare_end
 
-                priority_1_lower:
-                    PRINTF32 `SWAP FOR PRIORITY\n\n\n\x0`
-                    jmp swap_requests
-                priority_1_greater:
+                priority_1_equal: ; priority1 == priority2
+                    ; PRINTF32 `priority_1 == priority_2. Correct. Check for username\n\x0`
+
+                    ;; Comparing the requests' usernames
+                    mov DWORD [loop_index_3], 0
+                    loop_3_9:
+                        mov eax, [loop_index_3]
+                        mov bl, [username_1 + eax]
+                        cmp bl, [username_2 + eax]
+
+                        jb username_1_lower
+                        je username_1_equal
+                        ja username_1_greater
+                        jmp loop_3_9_continue
+
+                        username_1_lower: ; username1 < username2
+                            ; PRINTF32 `username_1 < username_2. Correct order. Nothing to do\n\x0`
+                            jmp compare_end
+                        username_1_greater: ; username1 > username2
+                            ; PRINTF32 `username_1 > username_2. Swapping...\n\x0`
+                            jmp swap_requests
+                        username_1_equal: ; username1 == username2
+                            ; PRINTF32 `username_1 == username_2. Correct order. Nothing to do\n\x0`
+                            jmp loop_3_9_continue
+
+                        loop_3_9_continue:
+
+                        inc dword [loop_index_3]
+                        cmp dword [loop_index_3], username_length
+                        jl loop_3_9
+
                     jmp compare_end
-
+                priority_1_lower: ; priority1 < priority2
+                    ; PRINTF32 `priority_1 < priority_2. Correct. Nothing to do\n\x0`
+                    jmp compare_end
+                priority_1_greater: ; priority1 > priority2
+                    ; PRINTF32 `priority_1 > priority_2. Swapping...\n\x0`
+                    ; PRINTF32 `SWAP FOR PRIORITY\n\x0`
+                    jmp swap_requests
                 jmp compare_end
+            admin_1_greater: ; admin1 > admin2
+                ; PRINTF32 `admin_1 > admin_2. Correct order. Nothing to do\n\x0`
+                jmp compare_end
+
             swap_requests:
+                ; PRINTF32 `Swaping {%d} with \x0`, [loop_index_1]
+                ; PRINTF32 `{%d}\n\x0`, [loop_index_2]
 
                 ;; sawp admin
                 mov al, [admin_1]
@@ -167,49 +217,103 @@ sort_requests:
 
                 ;; sawp username
                 mov DWORD [loop_index_3], 0
-                loop_3_6:
+                loop_3_3:
                     mov eax, [loop_index_3]
                     mov bl, [username_1 + eax]
                     mov [username_tmp + eax], bl
 
                     inc dword [loop_index_3]
                     cmp dword [loop_index_3], username_length
-                    jl loop_3_6
+                    jl loop_3_3
 
                 mov DWORD [loop_index_3], 0
-                loop_3_7:
+                loop_3_4:
                     mov eax, [loop_index_3]
                     mov bl, [username_2 + eax]
                     mov [username_1 + eax], bl
 
                     inc dword [loop_index_3]
                     cmp dword [loop_index_3], username_length
-                    jl loop_3_7
+                    jl loop_3_4
 
                 mov DWORD [loop_index_3], 0
-                loop_3_8:
+                loop_3_5:
                     mov eax, [loop_index_3]
                     mov bl, [username_tmp + eax]
                     mov [username_2 + eax], bl
 
                     inc dword [loop_index_3]
                     cmp dword [loop_index_3], username_length
-                    jl loop_3_8
+                    jl loop_3_5
 
                 ;; DEBUG ;; TODO Delete
-                PRINTF32 `[AFTER 1] Is admin? %hhd\n\x0`, [admin_1]
-                PRINTF32 `[AFTER 1] Priority: %hhu\n\x0`, [priority_1]
-                PRINTF32 `[AFTER 1] Passkey: %hu\n\x0`, [passkey_1]
-                PRINTF32 `[AFTER 1] Usename: %s\n\x0`, username_1
-                PRINTF32 `\nVS\n\n\x0`
+                ; PRINTF32 `After\n\x0`
+                ; PRINTF32 `Is admin? %hhd\n\x0`, [admin_1]
+                ; PRINTF32 `Priority: %hhu\n\x0`, [priority_1]
+                ; PRINTF32 `Passkey: %hu\n\x0`, [passkey_1]
+                ; PRINTF32 `Usename: %s\n\x0`, username_1
+                ; PRINTF32 `\nVS\n\n\x0`
 
-                PRINTF32 `[AFTER 2] Is admin? %hhd\n\x0`, [admin_2]
-                PRINTF32 `[AFTER 2] Priority: %hhu\n\x0`, [priority_2]
-                PRINTF32 `[AFTER 2] Passkey: %hu\n\x0`, [passkey_2]
-                PRINTF32 `[AFTER 2] Usename: %s\n\x0`, username_2
-                PRINTF32 `\n\n\n\n\x0`
+                ; PRINTF32 `Is admin? %hhd\n\x0`, [admin_2]
+                ; PRINTF32 `Priority: %hhu\n\x0`, [priority_2]
+                ; PRINTF32 `Passkey: %hu\n\x0`, [passkey_2]
+                ; PRINTF32 `Usename: %s\n\x0`, username_2
+                ; PRINTF32 `\n\n\n\n\x0`
+
+                ;; Copy request 1 to the main array
+                mov eax, [requests]
+
+                mov ebx, [loop_index_1]
+                imul ebx, struct_size
+                add eax, ebx
+
+                mov cl, [admin_1]
+                mov [eax], cl
+                mov cl, [priority_1]
+                mov [eax + 1], cl
+                mov cx, [passkey_1]
+                mov [eax + 2], cx
+
+                mov DWORD [loop_index_3], 0
+                loop_3_6:
+                    mov ebx, [loop_index_3]
+                    mov cl, [username_1 + ebx]
+                    mov [eax + ebx + 4], cl
+
+                    inc dword [loop_index_3]
+                    cmp dword [loop_index_3], username_length
+                    jl loop_3_6
 
 
+                ;; Copy request 2 to the main array
+                mov eax, [requests]
+
+                mov ebx, [loop_index_2]
+                imul ebx, struct_size
+                add eax, ebx
+
+                mov cl, [admin_2]
+                mov [eax], cl
+                mov cl, [priority_2]
+                mov [eax + 1], cl
+                mov cx, [passkey_2]
+                mov [eax + 2], cx
+
+                mov DWORD [loop_index_3], 0
+                loop_3_7:
+                    mov ebx, [loop_index_3]
+                    mov cl, [username_2 + ebx]
+                    mov [eax + ebx + 4], cl
+
+                    inc dword [loop_index_3]
+                    cmp dword [loop_index_3], username_length
+                    jl loop_3_7
+
+                mov eax, [requests]
+                mov ebx , [loop_index_1]
+
+                ;; DEBUG ;; TODO Delete
+                ; call debug_print
 
                 jmp compare_end
             compare_end:
@@ -221,6 +325,7 @@ sort_requests:
 
        inc dword [loop_index_1]
        mov ebx, [requests_length]
+       dec ebx
        cmp dword [loop_index_1], ebx
        jl loop_1
 
