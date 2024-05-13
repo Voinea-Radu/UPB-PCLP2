a0d2643d0cf2d0669ec81ac28778ab3e9f96d8b3
Voinea Radu <contact@voinearadu.com>
2024-05-13 08:28:54 +0300

    Finished bonus task

--- src/bonus/functional.asm
+++ src/bonus/functional.asm
@@ -1,31 +1,68 @@
-; Interpret as 64 bits code
 [bits 64]
 
-; nu uitati sa scrieti in feedback ca voiati
-; assembly pe 64 de biti
-
 section .text
-global map
-global reduce
+    global map
+    global reduce
+
+; void map(int64_t *dst, int64_t *src, int64_t n, int64_t(*f)(int64_t));
 map:
-    ; look at these fancy registers
     push rbp
     mov rbp, rsp
 
-    ; sa-nceapa turneu'
+    ; mov rdi, rdi ; dst
+    ; mov rsi, rsi ; src
+    ; mov rdx, rdx ; n
+    ; mov rcx, rcx ; f
+    mov r8, rdi
+
+    xor r9, r9
+    .loop:
+        cmp r9, rdx
+        je .loop_finish
+
+        mov rdi, [rsi + r9 * 8]
+        call rcx
+        mov rax, rax
+        mov [r8 + r9 * 8], rax
+
+        inc r9
+        jmp .loop
+
+    .loop_finish:
 
     leave
     ret
 
-
-; int reduce(int *dst, int *src, int n, int acc_init, int(*f)(int, int));
-; int f(int acc, int curr_elem);
+; int64_t reduce(int64_t *dst, int64_t *src, int64_t n, int64_t acc_init, int64_t(*f)(int64_t, int64_t));
 reduce:
-    ; look at these fancy registers
     push rbp
     mov rbp, rsp
 
-    ; sa-nceapa festivalu'
+    mov rdi, rdi ; dst ; ignored
+    mov rsi, rsi ; src
+    mov rdx, rdx ; n
+    mov rcx, rcx ; acc_init
+    mov r8, r8   ; f
+
+    mov r10, rsi
+    mov rdi, rcx
+
+    xor r9, r9
+    .loop:
+        cmp r9, rdx
+        je .loop_finish
+
+        push rdx
+        mov rsi, [r10 + r9 * 8]
+        call r8
+        pop rdx
+        mov rdi, rax
+
+        inc r9
+        jmp .loop
+
+    .loop_finish:
+    mov rax, rdi
 
     leave
     ret
