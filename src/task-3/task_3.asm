58d4ea9dc12da958361fcb35567de9cd1c755c42
Voinea Radu <contact@voinearadu.com>
2024-05-13 07:14:50 +0300

    Finished task 3

--- src/task-3/task_3.asm
+++ src/task-3/task_3.asm
@@ -1,41 +1,75 @@
 %include "../include/io.mac"
 
-; The `expand` function returns an address to the following type of data
-; structure.
 struc neighbours_t
-    .num_neighs resd 1  ; The number of neighbours returned.
-    .neighs resd 1      ; Address of the vector containing the `num_neighs` neighbours.
-                        ; A neighbour is represented by an unsigned int (dword).
+    .neighbour_count resd 1
+    .neighbours resd 1
 endstruc
 
 section .bss
-; Vector for keeping track of visited nodes.
-visited resd 10000
-global visited
+    visited resd 10000
+    global visited
 
 section .data
-; Format string for printf.
-fmt_str db "%u", 10, 0
+    format_string db "%u", 10, 0
+    node dd 0
+    expand dd 0
 
 section .text
-global dfs
-extern printf
+    global dfs
+    extern printf
 
-; C function signiture:
-;   void dfs(uint32_t node, neighbours_t *(*expand)(uint32_t node))
-; where:
-; - node -> the id of the source node for dfs.
-; - expand -> pointer to a function that takes a node id and returns a structure
-; populated with the neighbours of the node (see struc neighbours_t above).
-; 
-; note: uint32_t is an unsigned int, stored on 4 bytes (dword).
+;; void dfs(uint32_t node, neighbours_t *(*expand)(uint32_t node))
 dfs:
-    push ebp
-    mov ebp, esp
+    enter 0,0
+    pusha
 
-    ; TODO: Implement the depth first search algorith, using the `expand`
-    ; function to get the neighbours. When a node is visited, print it by
-    ; calling `printf` with the format string in section .data.
+    mov ebx, [ebp + 8]  ; node
+    mov ecx, [ebp + 12] ; expand
 
+    mov DWORD [node], ebx
+    mov DWORD [expand], ecx
+
+    push ebx
+    push format_string
+    call printf
+    add esp, 8
+
+    mov DWORD [visited + ebx * 4], 1
+
+    ; Free registers: eax, edx, esi, edi
+    ; neighbours_t *neighbours = expand(node);
+    mov ecx, DWORD [expand]
+    push ebx
+    call ecx
+    add esp, 4
+    ; eax = neighbours
+
+    mov edx, DWORD [eax + neighbours_t.neighbour_count]
+    mov esi, DWORD [eax + neighbours_t.neighbours]
+
+    mov edi, 0
+    .loop:
+        cmp edi, edx
+        jge .loop_end
+
+        mov ebx, DWORD [esi + edi * 4] ; neighbour
+
+        cmp DWORD [visited + ebx * 4], 0
+        je .not_visited
+        jmp .loop_continue
+
+        .not_visited:
+        mov ecx, DWORD [expand]
+        push ecx
+        push ebx
+        call dfs
+        add esp, 8
+
+        .loop_continue:
+        inc edi
+        jmp .loop
+
+    .loop_end:
+    popa
     leave
     ret
